14bed022e642e83b76a6f9a8b3c39823d76c11cb
김정하 <93295701+jeong-ha@users.noreply.github.com>
2022-04-27 18:20:12 +0900

    모바일 메뉴

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-    
+   //ad
+    $('.ad-btn').click(function(){
+        $('.ad, .ad-fix').hide();
+    });
+
+    //mobile menu
+    $('.m-menu-btn').click(function(){
+        $('.m-menu').show()
+        $('.m-menu-btn').hide()
+    });
+    $('.m-menu-close').click(function(){
+        $('.m-menu').hide()
+        $('.m-menu-btn').show()
+    });
    
     
     
